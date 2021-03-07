ff5346a51b1c05805040e5aa3593797bcef84890
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-07 18:54:46 +0100

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,6 +1,10 @@
 const discord = require("discord.js")
 
 exports.run = async (client, message, user, reaction, emoji ) => { 
+if (reaction.message.partial) await reaction.message.fetch();
+if (message.partial) await reaction.fetch();
+
+
 if (user.client) return;
 if (!reaction.message.guild) return;
 
