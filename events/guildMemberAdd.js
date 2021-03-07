e9e14e55420f14383e3ec90646734e0a51900a26
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-07 18:13:05 +0100

    init

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,8 +1,4 @@
 const discord = require("discord.js")
 
 exports.run = async (client, message ) => { 
-    myGuild = client.guilds.cache.get("796297446786334720");
-let memberCount = myGuild.memberCount;
-let memberCountChannel = myGuild.channels.cache.get("817375067012202496");
-memberCountChannel.setName(""+memberCount+ " Members")
 }
--- events/ready.js
+++ events/ready.js
@@ -11,10 +11,4 @@ var logEmbed = new discord.MessageEmbed()
 .setColor("#ff0000")
 .setDescription(`ready!`)
 .setTimestamp(); 
-var logsChannel = client.channels.cache.get("801837510820757514")
-logsChannel.send(logEmbed);
-    myGuild = client.guilds.cache.get("796297446786334720");
-let memberCount = myGuild.memberCount;
-let memberCountChannel = myGuild.channels.cache.get("817375067012202496");
-memberCountChannel.setName(""+memberCount+ " Members")
 }
