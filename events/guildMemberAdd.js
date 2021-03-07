e9cb74d008e88465dae59cd9f3497fb634027a7a
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-07 18:18:04 +0100

    init

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,8 @@
 const discord = require("discord.js")
 
 exports.run = async (client, message ) => { 
+    myGuild = client.guilds.cache.get("796297446786334720");
+let memberCount = myGuild.memberCount;
+let memberCountChannel = myGuild.channels.cache.get("817375067012202496");
+memberCountChannel.setName(""+memberCount+ " Members")
 }
