d75a92a159a4e03a42ec02267484ef7f7eea2fae
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-08 14:54:13 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,13 @@
 const discord = require("discord.js")
 
 exports.run = async (client, message, guildMemberAdd ) => { 
-user.send("hoi")
+    var logEmbed = new discord.MessageEmbed()
+.setTitle("New memeber | DDC")
+.setColor("#50a832")
+.setDescription(`member: <@${member.id}>`)
+.setTimestamp(); 
+var logsChannel = client.channels.cache.get("801837510820757514")
+logsChannel.send(logEmbed);
     myGuild = client.guilds.cache.get("796297446786334720");
     let memberCount = myGuild.memberCount;
     setInterval(function () {
