e406153d82b12211631b336e2d38d05ac86fb70f
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-08 14:35:30 +0100

    init

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -10,8 +10,7 @@ exports.run = async (client, message, guildMemberAdd ) => {
     logsChannel.send(logEmbed);
     const guild = client.guilds.cache.get("796297446786334720");
     setInterval(function () {
-       var memberCount = guild.members.filter(member => !member.user.bot).size;  
-       var memberCountChannel = client.channels.get("817375067012202496");
+       var memberCountChannel = client.channels.cache.get("817375067012202496");
        memberCountChannel.setName(`${guild.name} has ${memberCount} members!`);
     }, 1000);
 }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,8 +3,7 @@ const discord = require("discord.js")
 exports.run = async (client, message ) => { 
     const guild = client.guilds.cache.get("796297446786334720");
     setInterval(function () {
-       var memberCount = guild.members.filter(member => !member.user.bot).size;  
-       var memberCountChannel = client.channels.get("817375067012202496");
+       var memberCountChannel = client.channels.cache.get("817375067012202496");
        memberCountChannel.setName(`${guild.name} has ${memberCount} members!`);
     }, 1000);
 }
