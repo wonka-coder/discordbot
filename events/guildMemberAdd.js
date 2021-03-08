fdaf1b9688c1d1749aa7160d02504ec588e49ebc
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-08 10:09:27 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,15 @@
 const discord = require("discord.js")
 
 exports.run = async (client, message ) => { 
-    console.log("user leaved")
-    myGuild = client.guilds.cache.get("796297446786334720");
-let memberCount = myGuild.memberCount;
-let memberCountChannel = myGuild.channels.cache.get("817375067012202496");
-memberCountChannel.setName(""+memberCount+ " Members")
+    var logEmbed = new discord.MessageEmbed()
+    .setTitle("member joined | DDC ")
+    .setColor("#50a832")
+    .setDescription(`new member:${message.author} `)
+    .setTimestamp(); 
+    var logsChannel = client.channels.cache.get("801837510820757514")
+    logsChannel.send(logEmbed);
+        myGuild = client.guilds.cache.get("796297446786334720");
+    let memberCount = myGuild.memberCount;
+    let memberCountChannel = myGuild.channels.cache.get("817375067012202496");
+    memberCountChannel.setName(""+memberCount+ " Members")
 }
