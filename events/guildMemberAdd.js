a4e28777354084166d2e260a72c8bf7a00ff131d
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-08 14:33:47 +0100

    init

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,8 +8,10 @@ exports.run = async (client, message, guildMemberAdd ) => {
     .setTimestamp(); 
     var logsChannel = client.channels.cache.get("801837510820757514")
     logsChannel.send(logEmbed);
-    myGuild = client.guilds.cache.get("796297446786334720");
-    let memberCount = myGuild.memberCount;
-    let memberCountChannel = myGuild.channels.cache.get("817375067012202496");
-    memberCountChannel.setName(""+memberCount+ " Members")
+    const guild = client.guilds.cache.get("796297446786334720");
+    setInterval(function () {
+       var memberCount = guild.members.filter(member => !member.user.bot).size;  
+       var memberCountChannel = client.channels.get("817375067012202496");
+       memberCountChannel.setName(`${guild.name} has ${memberCount} members!`);
+    }, 1000);
 }
