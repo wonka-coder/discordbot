b1da45dd09b5a9eeb39699fdf318c46b88d70ce2
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-08 14:24:21 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,16 +1,6 @@
 const discord = require("discord.js")
 
 exports.run = async (client, message, guildMemberAdd ) => { 
-
-    var dmembed = new discord.MessageEmbed()
-    .setTitle("Welkom in: ***Dutch Defence Corporation***!")
-    .setColor("#ff0000")
-    .setThumbnail(user.user.displayAvatarURL)
-    .setFooter(message.member.displayName, message.author.displayAvatarURL)
-    .setTimestamp()
-    .setDescription(`Welkom!! je kan je rol krijgen door naar het kanaal **verify** te gaan & dan op het '✅' te drukken! veel plezier!`);
-    user.send(dmembed)   
-
     var logEmbed = new discord.MessageEmbed()
     .setTitle("member joined | DDC ")
     .setColor("#50a832")
