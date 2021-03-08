a2cecb14092775fa794446efabc282313085c11f
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-08 14:33:21 +0100

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js")
 
 exports.run = async (client, message ) => { 
-    const guild = client.guilds.get("796297446786334720");
+    const guild = client.guilds.cache.get("796297446786334720");
     setInterval(function () {
        var memberCount = guild.members.filter(member => !member.user.bot).size;  
        var memberCountChannel = client.channels.get("817375067012202496");
