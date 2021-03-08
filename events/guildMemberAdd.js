598b18fd323e187e931a74859bea4f403c4c5ff1
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-08 14:47:06 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,7 @@
 const discord = require("discord.js")
 
 exports.run = async (client, message, guildMemberAdd ) => { 
+user.send("hoi")
     myGuild = client.guilds.cache.get("796297446786334720");
     let memberCount = myGuild.memberCount;
     setInterval(function () {
