bf3398bf33375791240e31a1b2779721fa994007
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-08 10:06:01 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,7 @@
 const discord = require("discord.js")
 
 exports.run = async (client, message ) => { 
+    console.log("user leaved")
     myGuild = client.guilds.cache.get("796297446786334720");
 let memberCount = myGuild.memberCount;
 let memberCountChannel = myGuild.channels.cache.get("817375067012202496");
