ccd457e9e4e3ce31bedd0be9939c122437a5bae7
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-09 13:02:06 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -43,8 +43,8 @@ exports.run = (client, message, args) => {
         .then(message => {
           message = message.first()
           if (message.content.toUpperCase() == 'YES' || message.content.toUpperCase() == 'Y') {
-            User.send(dmEmbed).then(() =>
-            User.ban(reason).catch(err => {
+            User.send(dmembed).then(() =>
+            User.ban(reason)).catch(err => {
               if (err) return message.channel.send(`error : ${err}`);
             });
             message.reply(embed);
