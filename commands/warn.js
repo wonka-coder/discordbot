1f6448198bc01fa15b0096ba1dbdbeef35c57dfb
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-09 08:38:06 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -4,7 +4,7 @@ const warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (client, message, args) => {
   if (message.content == '!warn') {
-  message.delete(1000);
+   message.delete({ timeout: 3000 });
 
     if (!message.member.roles.cache.some(role => role.name === 'MODERATOR PERM')) return message.react("❌"), message.reply("you don't have the role:'MODERATOR PERM' !").then (message =>{
         message.delete({ timeout: 10000 })}), message.delete({ timeout: 3000 });
