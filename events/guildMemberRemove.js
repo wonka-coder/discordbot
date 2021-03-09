c40341a2f584732f1d30639faa8d9d8e879fbf6e
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-09 07:56:36 +0100

    init

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,10 +1,10 @@
 const discord = require("discord.js")
 
-exports.run = async (client, message ) => { 
+exports.run = async (client, message ) => {
     myGuild = client.guilds.cache.get("796297446786334720");
     let memberCount = myGuild.memberCount;
     setInterval(function () {
        var memberCountChannel = client.channels.cache.get("817375067012202496");
-       memberCountChannel.setName(`${memberCount} members!`);
+       memberCountChannel.setName(`👤| ${memberCount} members`);
     }, 10);
 }
--- events/ready.js
+++ events/ready.js
@@ -3,20 +3,20 @@ const { message } = require("noblox.js");
 
 const MIN_INTERVAL = 3 * 1000;
 
-exports.run = async client  => { 
+exports.run = async client  => {
     console.log(`${client.user.username} is online`)
     client.user.setActivity("Den Helder", {type: "WATCHING"});
 var logEmbed = new discord.MessageEmbed()
 .setTitle("ready! | Den helder")
 .setColor("#50a832")
 .setDescription(`ready!`)
-.setTimestamp(); 
+.setTimestamp();
 var logsChannel = client.channels.cache.get("801837510820757514")
 logsChannel.send(logEmbed);
 myGuild = client.guilds.cache.get("796297446786334720");
 let memberCount = myGuild.memberCount;
 setInterval(function () {
    var memberCountChannel = client.channels.cache.get("817375067012202496");
-   memberCountChannel.setName(`${memberCount} members!`);
+   memberCountChannel.setName(`👤| ${memberCount} members`);
 }, 1000);
 }
