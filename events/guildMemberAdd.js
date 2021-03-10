1db42adb3ed26b3e122eff765da3c4622d5dbfd9
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-10 17:01:24 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,6 +4,18 @@ const client = new discord.Client();
 
 exports.run = async (client, message, member, author ) => {
 
+  var logs = new discord.MessageEmbed()
+      .setColor("#ffbb00")
+      .setFooter("logs")
+      .setTimestamp()
+      .setDescription(`new memeber ${member.displayName}`)
+      .addField("**number of warnings:**", warns[warnUser.id].warns);
+
+  var channel = message.member.guild.channels.cache.get("801837510820757514");
+
+  if (!channel) return;
+
+  channel.send(logs);
   var embed = new discord.MessageEmbed()
     myGuild = client.guilds.cache.get("796297446786334720");
     let memberCount = myGuild.memberCount;
