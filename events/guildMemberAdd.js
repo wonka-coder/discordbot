d5b9f1975c168ddf048ce0cf77259d66a589333d
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-10 13:39:55 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,19 @@ const discord = require("discord.js")
 
 exports.run = async (client, message, member ) => {
 
+  var embed = new discord.MessageEmbed()
+      .setColor("#ff0000")
+      .setThumbnail(User.user.displayAvatarURL)
+      .setTitle("messages log | Den helder")
+      .setFooter("DDC | Logs")
+      .setTimestamp()
+      .setDescription(`** join members:**${message.author}`);
+      var channel = message.member.guild.channels.cache.get("801837510820757514");
+
+      if (!channel) return;
+
+      channel.send(embed);
+
     myGuild = client.guilds.cache.get("796297446786334720");
     let memberCount = myGuild.memberCount;
     setInterval(function () {
