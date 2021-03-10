8245d58756df04cafb6f2e6157e9ebc8b74dfed2
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-10 17:15:53 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,6 +3,11 @@ const discord = require("discord.js")
 const client = new discord.Client();
 
 exports.run = async (client, message, member, author ) => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
+
+if (!channel) return;
+
+channel.send(`Welcome to the server, ${member}`);
     myGuild = client.guilds.cache.get("796297446786334720");
     let memberCount = myGuild.memberCount;
     setInterval(function () {
