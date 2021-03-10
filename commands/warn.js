ace3e234c68c4874de056a7143e344a200fde489
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-10 16:03:25 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     if (!message.member.roles.cache.some(role => role.name === 'TRIAL MODERATOR PERM')) return message.react("❌"), message.reply("you don't have the role:'TRIAL MODERATOR PERM !").then (message =>{
         message.delete({ timeout: 10000 })}), message.delete({ timeout: 3000 });
 
-      if (!args[0]) return message.react("❌"), message.reply("no user specified").then (message =>{
+      if (!args[0]) return message.react("❌"), message.reply("no user specified!").then (message =>{
           message.delete({ timeout: 10000 })}), message.delete({ timeout: 3000 });
 
 
