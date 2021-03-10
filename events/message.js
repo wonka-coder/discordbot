8b28dd62f3eaadbf012f8a569306d1d99eb127be
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-10 18:35:03 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -13,6 +13,15 @@ if(!commandfile) return;
     commandfile.run(client,message,args);
   }
   if (message.channel.id === "798533810584551464") {
-message.reply(" the tickets are closed! | de tickets zijn dicht!")
+message.reply(" the tickets are closed! | de tickets zijn dicht!"), message.delete();
+  }
+  if (message.channel.id === "798533810584551464") {
+    var embed = new discord.MessageEmbed()
+        .setTitle("ticket closed! | DDC")
+        .setColor("#ff8000")
+        .setFooter("tickets!| Dutch Defence Corporation ")
+        .setTimestamp()
+        .setDescription(`the tickets are closed! if you have questions please send a message to <@464803075844997131> !  `)
+        message.channel.send(embed);
   }
 }
