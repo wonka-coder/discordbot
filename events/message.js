3833ca35c57ad1a0f679a8f2a10e686148b0bee3
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-10 18:20:49 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,15 +1,19 @@
-const prefix = "!"  
+const prefix = "!"
 
  exports.run = async(client, message) => {
   if (message.author.bot) return;
   if (message.content.startsWith(prefix)) {
-    
+
  let messageArray = message.content.split(" "),
      cmd = messageArray[0],
      args = messageArray.slice(1),
      commandfile = client.commands.get(cmd.slice(prefix.length)) || client.aliases.get(cmd.slice(prefix.length));
-  
-if(!commandfile) return;    
-    commandfile.run(client,message,args);             
+
+if(!commandfile) return;
+    commandfile.run(client,message,args);
+  }
+  if (message.channel.id === "798533810584551464") {
+message.reply(" the tickets are closed! | de tickets zijn dicht!")
+          });
   }
 }
