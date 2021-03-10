f970aa509c3ed4b87af50ed9c763dfa325fda133
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-10 15:54:00 +0100

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, messageDelete ) => {
     var logEmbed = new discord.MessageEmbed()
     .setTitle("messages log | DDC")
     .setColor("#ff0000")
-    .setDescription(`message deleted: **${message.cleanContent}** \n \n  message of: **${message.author}** \n \n message deleted from: **${message.channel}**`)
+    .setDescription(`**message deleted: **${message.cleanContent}** \n \n  message of: **${message.author}** \n \n message deleted from: **${message.channel}**`)
     .setTimestamp();
     var logsChannel = client.channels.cache.get("801837510820757514")
     logsChannel.send(logEmbed);
