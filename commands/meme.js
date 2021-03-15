526ec4d0b9e66ca7a9a264a459bad3c7f11772c1
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-16 00:41:09 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -8,7 +8,7 @@ exports.run = (client, message, args) => {
 fetch('https://www.reddit.com/r/memes/random/.json').then(resp => resp.json()).then(respMeme => {
 
 var permaL = respMeme[0].data.children[0].data.permaLink;
-var memeU = `https://www.reddit.com${permaLink}`;
+var memeU = `https://www.reddit.com${permaL}`;
 var memeF = respMeme[0].data.children[0].data.url;
 var memeT = respMeme[0].data.children[0].data.title;
 var memeUps = respMeme[0].data.children[0].data.ups;
