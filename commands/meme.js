2df545568e9ccc7b2a68243cbcd5278a2a23ff1d
Levi Meert <80203716+wonka-coder@users.noreply.github.com>
2021-03-16 00:35:45 +0100

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,34 @@
+const discord = require("discord.js");
+const fetch = require("node-fetch")
+ 
+const client = new discord.Client();
+
+exports.run = (client, message, args) => {
+
+fetch('https://www.reddir.com/r/memes/random.json').then(resp => resp.json()).then(respMeme => {
+
+var permaL = respMeme[0].data.children[0].data.permaLink;
+var memeU = `https://www.reddit.com${permaLink}`;
+var memeF = respMeme[0].data.children[0].data.url;
+var memeT = respMeme[0].data.children[0].data.title;
+var memeUps = respMeme[0].data.children[0].data.ups;
+var memeDowns = respMeme[0].data.children[0].data.downs;
+
+var embedMeme = new discord.MessageEmbed()
+.setTitle(`${memeT}`)
+.setUrl(`${memeU}`)
+.setImage(`${memeF}`)
+.setDescription(`👍 ${memeUps} \n 👎 ${memeDowns} `)
+
+message.channel.send(embedMeme);
+
+
+}).catch("error", (err) => {
+  console.log(err.message);
+})
+
+
+}
+exports.config = {
+  aliases: ["memes", "meme"]
+}
