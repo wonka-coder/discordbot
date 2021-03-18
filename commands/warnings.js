b8fee5f9b1b3f39d66b572e372e1e5c7a8d8b49f
wonka-coder <80203716+wonka-coder@users.noreply.github.com>
2021-03-18 22:26:16 +0100

    Update warnings.js

--- commands/warnings.js
+++ commands/warnings.js
@@ -7,37 +7,36 @@ const client = new discord.Client();
 const mongo = require('.././mongo')
 const warnSchema = require('.././schemas/warn-schema')
 module.exports.run = async (client, message, args) => {
-  callback: async (message, arguments, text) => {
-    const target = message.mentions.users.first()
-    if (!target) {
-      message.reply('Please specify a user to load the warnings for.')
-      return
-    }
+  const target = message.mentions.users.first()
+   if (!target) {
+     message.reply('Please specify a user to load the warnings for.')
+     return
+   }
 
-    const guildId = message.guild.id
-    const userId = target.id
+   const guildId = message.guild.id
+   const userId = target.id
 
-    await mongo().then(async (mongoose) => {
-      try {
-        const results = await warnSchema.findOne({
-          guildId,
-          userId,
-        })
+   await mongo().then(async (mongoose) => {
+     try {
+       const results = await warnSchema.findOne({
+         guildId,
+         userId,
+       })
 
-        let reply = `Previous warnings for <@${userId}>:\n\n`
+       let reply = `Previous warnings for <@${userId}>:\n\n`
 
-        for (const warning of results.warnings) {
-          const { author, timestamp, reason } = warning
+       for (const warning of results.warnings) {
+         const { author, timestamp, reason } = warning
 
-          reply += `By ${author} on ${new Date(
-            timestamp
-          ).toLocaleDateString()} for "${reason}"\n\n`
-        }
+         reply += `By ${author} on ${new Date(
+           timestamp
+         ).toLocaleDateString()} for "${reason}"\n\n`
+       }
 
-        message.reply(reply);
-
-      } finally {
-        mongoose.connection.close()
-      }
-    })
-  },
+       message.reply(reply)
+     } finally {
+       mongoose.connection.close()
+     }
+   })
+ },
+}
