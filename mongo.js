2ccb792ad7bce556daa3a7fc467c12c95486718d
wonka-coder <80203716+wonka-coder@users.noreply.github.com>
2021-03-18 18:50:58 +0100

    init

---
+++ mongo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const mongoPath = 'mongodb+srv://<Wonkatje>:<AsECekAJVl3FeSOf>@bot.k7div.mongodb.net/bot?retryWrites=true&w=majority'
+
+module.exports = async () => {
+  await mongoose.connect(mongoPath, )
+  userNewUrlParser: true,
+  useUnifiedToplogy: true,
+  )}
+  return mongoose
+
+}
