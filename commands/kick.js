c33352c5b15b650fcbb9d4d42ca14197a1ce1d4c
wonka-coder <80203716+wonka-coder@users.noreply.github.com>
2021-03-18 21:33:43 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -45,7 +45,7 @@ module.exports.run = async (client, message, args) => {
                 .then(message => {
             message = message.first()
               if (message.content.toUpperCase() == 'YES' || message.content.toUpperCase() == 'Y') {
-                target.kick(kickUser, { dagen:1, Reden: reason}).catch(err => {
+                target.kick(target, { dagen:1, Reden: reason}).catch(err => {
                   if (err) return message.channel.send(`error : ${err}`);
                 });
 
