74c2ec48f9fdb1333b4f1d769fd9748acbddd84d
wonka-coder <80203716+wonka-coder@users.noreply.github.com>
2021-03-19 14:58:00 +0100

    init

--- commands/ban.js
+++ commands/ban.js
@@ -3,70 +3,110 @@ const discord = require("discord.js");
 
 const client = new discord.Client();
 
-exports.run = (client, message, args) => {
+
+const mongo = require('.././mongo')
+const banSchema = require('.././schemas/ban-schema')
+module.exports.run = async (client, message, args) => {
   if(message && message.deletable) message.delete().catch(e => {});
 
-  if (!message.member.roles.cache.some(role => role.name === 'SUPER ADMIN PERM')) return message.react("❌"), message.reply("you don't have the role:: ``SUPER ADMIN PERM``!").then (message =>{
-    message.delete({ timeout: 10000 })}), message.delete({ timeout: 3000 });
+    if (!message.member.roles.cache.some(role => role.name === 'MODERATOR PERM')) return message.react("❌"), message.reply("you don't have the role:'MODERATOR PERM'!").then (message =>{
+      message.delete({ timeout: 10000 })}), message.delete({ timeout: 3000 });
 
-    if (!args[0]) return message.react("❌"), message.reply("no user speci.fied").then (message =>{
-        message.delete({ timeout: 10000 })}), message.delete({ timeout: 3000 });
-
-
+      if (!args[0]) return message.react("❌"), message.reply("no user specified!").then (message =>{
+          message.delete({ timeout: 10000 })}), message.delete({ timeout: 3000 });
 
 
-    var User = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
 
-    var reason = args.slice(1).join(" ");
+          const target = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
-    var embed = new discord.MessageEmbed()
-        .setColor("#ff0000")
-        .setThumbnail(User.user.displayAvatarURL)
-        .setFooter("his message was sent automatically because you received a ban in Dutch Defence Corporation. You can block the bot if you wish to stop receiving these messages")
-        .setTimestamp()
-        .setDescription(`** banned:** ${User} (${User.id})
-        **banned by:** ${message.author}
-        **reason: ** ${reason}`);
-        var channel = message.member.guild.channels.cache.get("801837510820757514");
+              if (!target) {
+                message.reply('Please specify someone to kick')
+                return
+              }
 
-        if (!channel) return;
+                  args.shift()
+              const guildId = message.guild.id
+              const userId = target.id
+              const reason = args.slice(1).join(' ')
+              const banned = {
+                author: message.member.user.tag,
+                timestamp: new Date().getTime(),
+                reason,
+              }
+              if (!target) return message.reply("user cannot be found!");
+              var embed = new discord.MessageEmbed()
+    .setColor("#ff0000")
+    .setThumbnail(User.user.displayAvatarURL)
+    .setFooter("his message was sent automatically because you received a ban in Dutch Defence Corporation. You can block the bot if you wish to stop receiving these messages")
+    .setTimestamp()
+    .setDescription(`** banned:** ${User} (${User.id})
+    **banned by:** ${message.author}
+    **reason: ** ${reason}`);
+    var channel = message.member.guild.channels.cache.get("801837510820757514");
 
-        channel.send(embed);
-        var dmembed = new discord.MessageEmbed()
-        .setTitle("You have been banned from ***Dutch Defence Corporation***!")
-        .setColor("#ff0000")
-        .setThumbnail(User.user.displayAvatarURL)
-        .setFooter(message.member.displayName, message.author.displayAvatarURL)
-        .setTimestamp()
-        .setDescription(` you have been banned for the following reason: **${reason}** \n You can get an unban by clicking the link below! \n *https://forms.gle/souGHuLT83G1URVu5*`);
+    if (!channel) return;
 
-    let filter = m => m.author.id === message.author.id
-    message.channel.send(`Do you want ${User} banned from the server for ${reason} \`YES\` / \`NO\``).then(() => {
-      message.channel.awaitMessages(filter, {
-          max: 1,
-          time: 30000,
-          errors: ['time']
-        })
-        .then(message => {
-          message = message.first()
-          if (message.content.toUpperCase() == 'YES' || message.content.toUpperCase() == 'Y') {
-            User.send(dmembed).then(() =>
-            User.ban(User, { dagen:1, Reden: reason})).catch(err => {
-              if (err) return message.channel.send(`error : ${err}`);
-            });
-            message.reply(embed);
-          } else if (message.content.toUpperCase() == 'NO' || message.content.toUpperCase() == 'N') {
-             message.reply("ban canceled!");
-          } else {
-            message.channel.send(`command stopped`)
-          }
-        })
-        .catch(collected => {
-            message.channel.send('time over');
-        });
+    channel.send(embed);
 
-    })
-}
+    var dmembed = new discord.MessageEmbed()
+ .setTitle("You have been banned from ***Dutch Defence Corporation***!")
+ .setColor("#ff0000")
+ .setThumbnail(User.user.displayAvatarURL)
+ .setFooter(message.member.displayName, message.author.displayAvatarURL)
+ .setTimestamp()
+ .setDescription(` you have been banned for the following reason: **${reason}** \n You can get an unban by clicking the link below! \n *https://forms.gle/souGHuLT83G1URVu5*`);
+
+              let filter = m => m.author.id === message.author.id
+              message.channel.send(`Do you want ${target} BAN the server for ${reason} \`YES\` / \`NO\``).then(() => {
+                message.channel.awaitMessages(filter, {
+                  max: 1,
+                  time: 30000,
+                  errors: ['time']
+                })
+                .then(message => {
+            message = message.first()
+              if (message.content.toUpperCase() == 'YES' || message.content.toUpperCase() == 'Y') {
+                  target.send(dmembed).then(() =>
+                target.kick(target, { dagen:1, Reden: reason}).catch(err => {
+                  if (err) return message.channel.send(`error : ${err}`);
+                });
+
+           message.reply(embed);
+         } else if (message.content.toUpperCase() == 'NO' || message.content.toUpperCase() == 'N') {
+            message.reply("banned canceled");
+         } else {
+           message.channel.send(`Command stopped`)
+         }
+       })
+       .catch(collected => {
+           message.channel.send('time over');
+       });
+
+   })
+
+              await mongo().then(async (mongoose) => {
+                try {
+                  await banSchema.findOneAndUpdate(
+                    {
+                      guildId,
+                      userId,
+                    },
+                    {
+                      guildId,
+                      userId,
+                      $push: {
+                        bans: banned,
+                      },
+                    },
+                    {
+                      upsert: true,
+                    }
+                  )
+                } finally {
+                  mongoose.connection.close()
+                }
+              })
+            },
   exports.config = {
-    aliases: ["BAN", "BAN_MEMBER"]
+    aliases: ["KICK", "KICK_MEMBER"]
   }
---
+++ schemas/ban-schema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const kickSchema = mongoose.Schema({
+  guildId: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  kicks: {
+    type: [Object],
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('kicks', kickSchema)
