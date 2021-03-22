5424ffcc0558989d3ab7144c35458b03469e3b6d
wonka-coder <80203716+wonka-coder@users.noreply.github.com>
2021-03-22 14:57:36 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,7 +3,7 @@ const discord = require("discord.js")
 const client = new discord.Client();
 
 exports.run = async (client, message, member, author, guildMember ) => {
-  var roleMember = message.guild.roles.find(roleMember => roleMember.name === "Member");
+  var roleMember = message.guild.roles.cache.find(roleMember => roleMember.name === "Member");
   message.member.addRole(roleMember);
     myGuild = client.guilds.cache.get("796297446786334720");
     let memberCount = myGuild.memberCount;
