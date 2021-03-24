8c60ae30988c27fde20224f3659934e3506fea6b
wonka-coder <80203716+wonka-coder@users.noreply.github.com>
2021-03-24 12:48:42 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -8,7 +8,7 @@ const mongo = require('.././mongo')
 const warnSchema = require('.././schemas/warn-schema')
 
 
-module.exports.run = async (client, message, member, args) => {
+module.exports.run = async (client, member, args) => {
   const target = message.mentions.users.first()
       if (!target) {
         message.reply('Please specify someone to warn.')
