ba234f68a69bcb5fb19950829cf413d93a093d26
wonka-coder <80203716+wonka-coder@users.noreply.github.com>
2021-04-02 15:56:15 +0200

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -4,29 +4,32 @@ const discord = require("discord.js");
 const client = new discord.Client();
 
 exports.run = async (client, message, args) => {
-
+   const categoryId = "806092431698427984";
   var ticketC = "798533810584551464";
 
   if(ticketC === message.channel.id){} else {
               return message.channel.send("You can not make a ticket here! ")
           }
 
-  if(message.guild.channels.cache.find(channel => channel.name === `ticket-${message.author.id}`)) {
+  if(message.guild.channels.cache.find(channel => channel.name == userName.toLowerCase() + "-" + userDiscriminator)) {
   			return message.reply('you already have a ticket, please close your exsisting ticket first before opening a new one!');
   		}
 
-  		message.guild.channels.create(`ticket-${message.author.id}`, {
-        // Put permissions for everyone
-        overwritePermissions(message.guild.roles.cache.find('name', "@everyone"), { "READ_MESSAGES": false });
-        overwritePermissions(message.guild.roles.cache.find('name', "@support"), { "VIEW_CHANNEL": true });
-        // Put permission by the user that created the ticket
-        overwritePermissions(message.author, {
+      message.guild.createChannel(userName + "-" + userDiscriminator, "text").then((createdChan) => { // Maak kanaal
 
-            "READ_MESSAGES": true, "SEND_MESSAGES": true,
-            "ATTACH_FILES": true, "CONNECT": true,
-            "CREATE_INSTANT_INVITE": false, "ADD_REACTIONS": true
+             createdChan.setParent(categoryId).then((settedParent) => { // Zet kanaal in category.
 
-        }).then(async channel => {
+                 // Put permissions for everyone
+                 settedParent.overwritePermissions(message.guild.roles.find('name', "@everyone"), { "READ_MESSAGES": false });
+                 settedParent.overwritePermissions(message.guild.roles.find('name', "@support"), { "VIEW_CHANNEL": true });
+                 // Put permission by the user that created the ticket
+                 settedParent.overwritePermissions(message.author, {
+
+                     "READ_MESSAGES": true, "SEND_MESSAGES": true,
+                     "ATTACH_FILES": true, "CONNECT": true,
+                     "CREATE_INSTANT_INVITE": false, "ADD_REACTIONS": true
+
+                 }).then(async channel => {
   			message.reply(`you have successfully created a ticket! Please click on ${channel} to view your ticket.`);
   			channel.send(`Hi ${message.author}, welcome to your ticket! Please be patient, we will be with you shortly. If you would like to close this ticket please run \`?close\``);
   			let logchannel = message.guild.channels.cache.find(channel => channel.name === `den-helderbot-logs`)
