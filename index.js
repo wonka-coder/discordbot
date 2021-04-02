c0678827172100ddb0b7743e5ac49984fd11ace4
wonka-coder <80203716+wonka-coder@users.noreply.github.com>
2021-04-02 18:20:12 +0200

    init

--- index.js
+++ index.js
@@ -50,5 +50,22 @@ client.on("message", async message => {
 
 
 });
+async function promptMessage(message, author, time, reactions) {
+// We gaan eerst de tijd * 1000 doen zodat we seconden uitkomen.
+time *= 1000;
+
+// We gaan ieder meegegeven reactie onder de reactie plaatsen.
+for (const reaction of reactions) {
+    await message.react(reaction);
+}
+
+// Als de emoji de juiste emoji is die men heeft opgegeven en als ook de auteur die dit heeft aangemaakt er op klikt
+// dan kunnen we een bericht terug sturen.
+const filter = (reaction, user) => reactions.includes(reaction.emoji.name) && user.id === author.id;
+
+// We kijken als de reactie juist is, dus met die filter en ook het aantal keren en binnen de tijd.
+// Dan kunnen we bericht terug sturen met dat icoontje dat is aangeduid.
+return message.awaitReactions(filter, { max: 1, time: time }).then(collected => collected.first() && collected.first().emoji.name);
+}
 
 client.login(process.env.token);
