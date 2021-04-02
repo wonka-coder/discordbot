d4f7785f8d1ac540d319a3c107a5d4a8352b6032
wonka-coder <80203716+wonka-coder@users.noreply.github.com>
2021-04-02 14:55:39 +0200

    Create ticket.js

---
+++ commands/ticket.js
@@ -0,0 +1,40 @@
+const discord = require("discord.js");
+
+
+const client = new discord.Client();
+
+exports.run = async (client, message, args) => {
+  var ticketC = "798533810584551464";
+
+  if(ticketC === message.channel.id){} else {
+              return message.channel.send("You can not make a ticket here! ")
+          }
+
+  if(message.guild.channels.cache.find(channel => channel.name === `ticket-${message.author.id}`)) {
+  			return message.reply('you already have a ticket, please close your exsisting ticket first before opening a new one!');
+  		}
+
+  		message.guild.channels.create(`ticket-${message.author.id}`, {
+  			permissionOverwrites: [
+  				{
+  					id: message.author.id,
+  					allow: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
+  				},
+  				{
+  					id: message.guild.roles.everyone,
+  					deny: ['VIEW_CHANNEL'],
+  				},
+  			],
+  			type: 'text',
+  		}).then(async channel => {
+  			message.reply(`you have successfully created a ticket! Please click on ${channel} to view your ticket.`);
+  			channel.send(`Hi ${message.author}, welcome to your ticket! Please be patient, we will be with you shortly. If you would like to close this ticket please run \`${prefix}close\``);
+  			let logchannel = message.guild.channels.cache.find(channel => channel.name === `den-helderbot-logs`)
+  			if(logchannel) {
+  				logchannel.send(`Ticket ${message.author.id} created. Click the following to veiw <#${channel.id}>`);
+  			}
+  		});
+  	},
+  exports.config = {
+    aliases: ["ticket open", "everyone"]
+  }
