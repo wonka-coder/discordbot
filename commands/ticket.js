4b315b3da20bd4de239d6671d3ab9cd2d4971cb3
wonka-coder <80203716+wonka-coder@users.noreply.github.com>
2021-04-02 18:15:30 +0200

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -4,7 +4,6 @@ var emoij = ("flag_nl", "flag_us");
 const client = new discord.Client();
 
 exports.run = async (client, message, args) => {
-var emoij = (":flag_nl:", ":flag_us:");
   var embedNL = new discord.MessageEmbed()
 .setTitle("Ticket systeem |  ***Dutch Defence Corporation***!")
 .setColor("#ff0000")
@@ -47,7 +46,7 @@ var embedPrompt = new discord.MessageEmbed()
 
         message.channel.send(embedPrompt).then(async msg => {
 
-                    var emoji = await promptMessage();
+                    var emoji = await promptMessage(":flag_nl:", ":flag_us:"");
 
                     if (emoji === " :flag_nl:") {
 
@@ -59,13 +58,30 @@ var embedPrompt = new discord.MessageEmbed()
 
                     }
 
-                }); 
+                });
   			let logchannel = message.guild.channels.cache.find(channel => channel.name === `den-helderbot-logs`)
   			if(logchannel) {
   				logchannel.send(`Ticket ${message.author.id} created. Click the following to veiw <#${channel.id}>`);
   			}
   		});
   	},
+    async function promptMessage(message, author, time, reactions) {
+    // We gaan eerst de tijd * 1000 doen zodat we seconden uitkomen.
+    time *= 1000;
+
+    // We gaan ieder meegegeven reactie onder de reactie plaatsen.
+    for (const reaction of reactions) {
+        await message.react(reaction);
+    }
+
+    // Als de emoji de juiste emoji is die men heeft opgegeven en als ook de auteur die dit heeft aangemaakt er op klikt
+    // dan kunnen we een bericht terug sturen.
+    const filter = (reaction, user) => reactions.includes(reaction.emoji.name) && user.id === author.id;
+
+    // We kijken als de reactie juist is, dus met die filter en ook het aantal keren en binnen de tijd.
+    // Dan kunnen we bericht terug sturen met dat icoontje dat is aangeduid.
+    return message.awaitReactions(filter, { max: 1, time: time }).then(collected => collected.first() && collected.first().emoji.name);
+}
   exports.config = {
     aliases: ["ticket open", "everyone"]
   }
